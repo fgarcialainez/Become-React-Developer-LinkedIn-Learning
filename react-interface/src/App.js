ea024f743b7f2425e79934e04810fc1f520cc3d2
Felix Garcia Lainez <fgarcialainez@gmail.com>
2021-01-02 12:30:05 +0100

    Completed Lesson 1: Building a Basic App

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main class="page bg-white" id="petratings">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 bg-white">
+          <div class="container">
+            <div>Add Appointments</div>
+            <div>Search Appointments</div>
+            <div>List Appointments</div>
+          </div>
+        </div>
+      </div>
     </div>
+  </main>
   );
 }
 
